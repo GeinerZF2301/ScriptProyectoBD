71dc586bfbeee0e6201b62909196ee761507ae24
Geiner ZF <gazfyngg@gmail.com>
2022-06-17 11:44:42 -0600

    Se renombra el archivo y se dividen

--- SQLQueryProyectoBD_CreacionTablas.sql
+++ SQLQueryProyectoBD_CreacionTablas.sql
@@ -2,101 +2,6 @@
 Integrantes: Maria del Mar Acón Vargas, Geiner Zúñiga Flores, Kriscia Campos Cárdenas,
 			 Melany Lawson Mendez, Alfieri Mora Jiménez*/
 
-Use Master 
-go
-Create Database ConservatorioCastella
---Archivo Primario
-On Primary
-(
-Name='CoservatorioCastella_Data',
-FIlename='C:\SQLData\CoservatorioCastella_Data.mdf',
-size=60Mb,
-maxsize=80Mb,
-filegrowth=2Mb)
---Archivo de Registro
-Log On
-(
-Name='CoservatorioCastella_Log',
-FIlename='C:\SQLLOG\CoservatorioCastella_Log.ldf',
-size=50Mb,
-maxsize=60Mb,
-filegrowth=5Mb
-)
-Go
----Archivo Secundario
-Use Master
-Go
-Alter Database ConservatorioCastella
-Add file
-(
-Name='CoservatorioCastella_sec_Data',
-FIlename='C:\SQLLOG\CoservatorioCastella_sec_Data.ndf',
-size=40Mb,
-maxsize=50Mb,
-filegrowth=4Mb
-)
-Go
-
----Creación de Filegroups
-Use master
-Go
-Alter Database ConservatorioCastella
-Add Filegroup Personas
-Go
-
-Use MAster
-Go
-Alter Database ConservatorioCastella
-Add FIlegroup Administracion
-Go
-
-Use MAster
-Go
-Alter Database ConservatorioCastella
-Add FIlegroup Infraestructura
-Go
-
-
---Se crea el archivo de datos para el Filegroup Personas
-Use Master
-GO
-Alter database ConservatorioCastella
-Add file
-(Name='Personas_Data',
-Filename='C:\SQLData\Personas_Data.ndf',
-Size=20Mb,
-Maxsize=30Mb,
-Filegrowth=4Mb
-)
-TO FILEGROUP Personas
-GO
-
---Se crea el archivo de datos para el Filegroup Infraestructura
-Use Master
-Go
-Alter database ConservatorioCastella
-Add file
-(Name='Infraestructura_Data',
-Filename='C:\SQLData\Infraestructura.ndf',
-Size=30Mb,
-Maxsize=35Mb,
-Filegrowth=4Mb
-)
-TO FILEGROUP Infraestructura
-GO
-
---Se crea el archivo de datos para el Filegroup Administracion
-Use Master
-Alter database ConservatorioCastella
-Add file
-(Name='Administraticion_Data',
-Filename='C:\SQLData\Administrativo.ndf',
-Size=30Mb,
-Maxsize=35Mb,
-Filegrowth=4Mb
-)
-TO FILEGROUP Administracion
-GO
 
 --Ver el estado de la base de datos
 sp_helpDb ConservatorioCastella
@@ -110,7 +15,8 @@ sp_helpfilegroup
 /*En este apartado se crean las tablas de la base de datos y se asignan 
 al filegroup personas*/
 
---Creacion de la tabla Encargado
+
+--Tabla ENCARGADO
 Use ConservatorioCastella
 GO
 Create table Encargado(
@@ -126,7 +32,7 @@ Go
 Sp_help Encargado
 go
 
---Creacion de la tabla Alumno
+--Tabla ALUMNO
 Use ConservatorioCastella
 GO
 Create table Alumno(
@@ -144,7 +50,7 @@ PRIMARY KEY(Dni_Alumno),
 FOREIGN KEY(FK_Dni_Encargado) REFERENCES Encargado(Dni_Encargado)
 ) ON Personas
 Go
-
+--Tabla PROFESOR
 Use ConservatorioCastella
 GO
 Create table Profesor(
@@ -160,10 +66,12 @@ PRIMARY KEY(Dni_Profesor),
 GO
 
 
-/*En este apartado se crean las tablas que se consideran parte administrativa o de interes
+
+
+/*En este apartado se crean las tablas que se consideran parte ADMINISTRATIVA o de interes
 del conservatorio, se añaden al Filegroup Administracion*/
 
---Creacion de la tabla Tipo
+--Tabla TIPO
 Use ConservatorioCastella
 GO
 Create Table Tipo(
@@ -174,7 +82,7 @@ Create Table Tipo(
  )ON Administracion
  GO
 
- --Creacion de la tabla Instrumento
+ --Tabla INSTRUMENTO
 Use ConservatorioCastella
 GO
 Create Table Instrumento(
@@ -189,7 +97,40 @@ Create Table Instrumento(
 )ON Administracion
 GO
 
---Creacion de la tabla AlumnoInstrumento
+
+---Tabla CURSO
+
+Use ConservatorioCastella
+GO
+Create table Curso(
+Año int NOT NULL,
+Fecha_Inicio date NOT NULL,
+Fecha_Final date NOT NULL
+Constraint PK_Curso
+PRIMARY KEY(Año)
+) ON Administracion
+GO
+
+
+--Tabla ASIGNATURA INSTRUMENTAL
+Use ConservatorioCastella
+GO
+Create Table AsignaturaInstrumental(
+Codigo_AsignaturaInstrumental varchar(20) NOT NULL,
+Nombre varchar(30) NOT NULL,
+CantidadHoras int NOT NULL,
+FK_IdAulaInstrumental varchar(30) Not Null,
+Constraint PK_AsignaturaInstrumental
+PRIMARY KEY(Codigo_AsignaturaInstrumental),
+FOREIGN KEY(FK_IdAulaInstrumental) REFERENCES AulasInstrumentales(Id_AulaInstrumental)
+--Falta añadir la relacion con la llave foránea 
+)ON Administracion
+
+go
+
+
+
+--Tabla ALUMNO INSTRUMENTO
 Use ConservatorioCastella
 GO
 Create Table AlumnoInstrumento(
@@ -206,30 +147,18 @@ GO
 
 
 
---Creacion de la tabla Asignatura Instrumental
-Use ConservatorioCastella
-GO
-Create Table AsignaturaInstrumental(
-Codigo_AsignaturaInstrumental varchar(20) NOT NULL,
-Nombre varchar(30) NOT NULL,
-CantidadHoras int NOT NULL,
-FK_IdAulaInstrumental varchar(10)
-Constraint PK_AsignaturaInstrumental
-PRIMARY KEY(Codigo_AsignaturaInstrumental)
---Falta añadir la relacion con la llave foránea 
-)ON Administracion
-go
 
---Creacion de la tabla Asignatura No Instrumental
+--Tabla ASIGNATURA MAGISTRAL
 Use ConservatorioCastella
 GO
-Create Table AsignaturaMagistal(
+Create Table AsignaturaMagistral(
 Codigo_AsignaturaMagistral varchar(20) NOT NULL,
 Nombre varchar(30) NOT NULL,
 CantidadHoras int NOT NULL,
-FK_IdAulaMagistral varchar(10)
+FK_IdAulaMagistral varchar(30),
 Constraint PK_AsignaturaNoInstrumental
-PRIMARY KEY(Codigo_AsignaturaMagistral)
+PRIMARY KEY(Codigo_AsignaturaMagistral),
+FOREIGN KEY(FK_IdAulaMagistral) REFERENCES AulasMagistrales(Id_AulaMagistral)
 
 )ON Administracion
 go
@@ -241,8 +170,6 @@ Create Table AlumnoAsignaturaInstrumental(
 Id_AlumnoAsignatura INTEGER IDENTITY(1,1) NOT NULL,
 FK_DniAlumno varchar(10) NOT NULL,
 FK_CodigoAsignaturaInstrumental varchar(20) NOT NULL,
-Constraint PK_Id_AlumnoAsignatura
-PRIMARY KEY(Id_AlumnoAsignatura),
 FOREIGN KEY(FK_DniAlumno) REFERENCES Alumno(Dni_Alumno),
 FOREIGN KEY(FK_CodigoAsignaturaInstrumental) REFERENCES AsignaturaInstrumental(Codigo_AsignaturaInstrumental)
 ) ON Administracion
@@ -254,25 +181,55 @@ GO
 Create Table AlumnoAsignaturaMagistral(
 Id_AlumnoAsignatura INTEGER IDENTITY(1,1) NOT NULL,
 FK_DniAlumno varchar(10) NOT NULL,
-FK_CodigoAsignaturaNoInstrumental varchar(20) NOT NULL,
+FK_CodigoAsignaturaMagistral varchar(20) NOT NULL,
 FOREIGN KEY(FK_DniAlumno) REFERENCES Alumno(Dni_Alumno),
-FOREIGN KEY(FK_CodigoAsignaturaNoInstrumental) REFERENCES AsignaturaMagistral(Codigo_AsignaturaMagistral)
+FOREIGN KEY(FK_CodigoAsignaturaMagistral) REFERENCES AsignaturaMagistral(Codigo_AsignaturaMagistral)
 ) ON Administracion
 GO
 
+
+---Tabla ENSAYO RESERVA
 Use ConservatorioCastella
 GO
-Create table Curso(
-Año int NOT NULL,
-Fecha_Inicio date NOT NULL,
-Fecha_Final date NOT NULL
-Constraint PK_Curso
-PRIMARY KEY(Año)
-) ON Administracion
+ Create Table EnsayoReserva
+ (
+   Codigo_EnsayoReserva   varchar(30) Not NUll,
+   Hora_Inicio            Time NUll,
+   Hora_Fin               Time NUll,
+   Fecha                  Date Null,
+   Dni_Profesor           varchar(10) Not NUll,
+   Dni_Alumno             varchar(10) Not NUll
+   Constraint PK_EnsayoReserva
+   Primary Key Clustered (Codigo_EnsayoReserva)
+   Foreign Key (Dni_Profesor) References Profesor(Dni_Profesor),
+   Foreign Key (Dni_Alumno) References Alumno (Dni_Alumno)
+
+ )ON Administracion
+ Go
+
+ --Tabla INSTRUMENTO PROFESOR
+ 
+Use ConservatorioCastella
 GO
+ Create Table InstrumentoProfesor(
+   Id_InstrumentoProfesor INTEGER IDENTITY(1,1) NOT NULL,
+   Id_Instrumento         int NOT NULL,    
+   Dni_Profesor           varchar(10) Not NUll
+   Constraint PK_InstrumentoProfesor
+   Primary Key Clustered (Id_InstrumentoProfesor)
+   Foreign Key (Id_Instrumento) References Instrumento (Id_Instrumento),
+   Foreign Key (Dni_Profesor) References Profesor(Dni_Profesor)
+
+ )On Administracion
+ GO
 
 
-/*Creacion de las tablas de Aulas y Edificios que van al filegroup de Infraestructura*/
+
+
+
+
+
+ /*Creacion de las tablas de Aulas y Edificios que van al filegroup de Infraestructura*/
 
 Use ConservatorioCastella
 GO
@@ -305,38 +262,22 @@ Id_AulaInstrumental varchar(30) NOT NULL,
 Metros_Poseidos int NOT NULL,
 Nombre varchar(20) NULL,
 Aforo int NOT NULL,
+FK_Id_Instrumento int NOT NULL,
 FK_CodigoEdificio varchar(15) NOT NULL,
 FK_CodigoAsignaturaInstrumental varchar(20) NOT NULL,
 Constraint PK_AulasInstrumentales
 PRIMARY KEY(Id_AulaInstrumental),
+FOREIGN KEY(FK_Id_Instrumento) REFERENCES Instrumento(Id_Instrumento),
 FOREIGN KEY(FK_CodigoEdificio) REFERENCES Edificio(Codigo_Edificio),
-FOREIGN KEY(FK_CodigoAsignaturaInstrumental) REFERENCES AsignaturaInstrumental(Codigo_AsignaturaInstrumental)
 ) ON Infraestructura
 GO
 
 
-Use ConservatorioCastella
-GO
- Create Table EnsayoReserva(
-   Codigo_EnsayoReserva   varchar(30) Not NUll,
-   Hora_Inicio            Time NUll,
-   Hora_Fin               Time NUll,
-   Fecha                  Date Null,
-   Dni_Profesor           varchar(10) Not NUll,
-   Dni_Alumno             varchar(10) Not NUll
-   Constraint PK_EnsayoReserva
-   Primary Key Clustered (Codigo_EnsayoReserva)
-   Foreign Key (Dni_Profesor) References Profesor(Dni_Profesor),
-   Foreign Key (Dni_Alumno) References Alumno (Dni_Alumno)
- ) ON Administracion
-	GO
 
- 
-Use ConservatorioCastella
-GO
- Create Table InstrumentoProfesor(
-   Id_InstrumentoProfesor INTEGER IDENTITY(1,1) NOT NULL,
 
- )
 
- Drop Database ConservatorioCastella
+
+
+
+
+
